27dc5781860e3728e168b76ce00f3ea6c9b7ac51
Pavel Tížek <pavel@tizek.cz>
2022-05-31 14:38:37 +0200

    Upteted extension

--- src/Kollarovic/Thumbnail/DI/Extension.php
+++ src/Kollarovic/Thumbnail/DI/Extension.php
@@ -2,6 +2,7 @@
 
 namespace Kollarovic\Thumbnail\DI;
 
+use Latte\Engine;
 use Nette;
 
 
@@ -13,32 +14,35 @@ use Nette;
 class Extension extends Nette\DI\CompilerExtension
 {
 
-	public function loadConfiguration()
-	{
-		$builder = $this->getContainerBuilder();
+    public function loadConfiguration()
+    {
+        $builder = $this->getContainerBuilder();
 
-		$defaults = [
-			'wwwDir' => $builder->parameters['wwwDir'],
-			'httpRequest' => '@httpRequest',
-			'thumbPathMask' => 'images/thumbs/{filename}-{width}x{height}.{extension}',
-			'placeholder' => 'http://dummyimage.com/{width}x{height}/efefef/f00&text=Image+not+found',
-			'filterName' => 'thumbnail',
-		];
+        $defaults = [
+            'wwwDir' => $builder->parameters['wwwDir'],
+            'httpRequest' => '@httpRequest',
+            'thumbPathMask' => 'images/thumbs/{filename}-{width}x{height}.{extension}',
+            'placeholder' => 'http://dummyimage.com/{width}x{height}/efefef/f00&text=Image+not+found',
+            'filterName' => 'thumbnail',
+        ];
 
-		$config = $this->validateConfig($defaults);
+        $config = $this->validateConfig($defaults);
 
-		$builder->addDefinition($this->prefix('thumbnail'))
-			->setFactory('Kollarovic\Thumbnail\Generator', array(
-				'wwwDir' => $config['wwwDir'],
-				'httpRequest' => $config['httpRequest'],
-				'thumbPathMask' => $config['thumbPathMask'],
-				'placeholder' => $config['placeholder']
-			));
+        $builder->addDefinition($this->prefix('thumbnail'))
+            ->setFactory('Kollarovic\Thumbnail\Generator', array(
+                'wwwDir' => $config['wwwDir'],
+                'httpRequest' => $config['httpRequest'],
+                'thumbPathMask' => $config['thumbPathMask'],
+                'placeholder' => $config['placeholder']
+            ));
 
-		if ($builder->hasDefinition('nette.latteFactory')) {
-			$definition = $builder->getDefinition('nette.latteFactory');
-			$definition->addSetup('addFilter', array($config['filterName'], array($this->prefix('@thumbnail'), 'thumbnail')));
-		}
-	}
+        if ($builder->hasDefinition('nette.latteFactory')) {
+            $builder->addFactoryDefinition($this->prefix("latteFactory"))
+                ->setImplement(Nette\Bridges\ApplicationLatte\ILatteFactory::class)
+                ->getResultDefinition()
+                ->setFactory(Engine::class)
+                ->addSetup('addFilter', array($config['filterName'], array($this->prefix('@thumbnail'), 'thumbnail')));
+        }
+    }
 
 }
